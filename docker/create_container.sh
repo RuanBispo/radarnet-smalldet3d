9d1ef1cd6b631b8c93d41fac56c2e3910034ae84
RuanBispo <61762635+RuanBispo@users.noreply.github.com>
2024-07-29 11:33:56 +0100

    Update and rename run.sh to create_container.sh

--- docker/create_container.sh
+++ docker/create_container.sh
@@ -1,7 +1,7 @@
 docker run \
   -it \
   --gpus all \
-  -v "${PWD}/../:/code" \
+  -v "${PWD}/:/code" \
   -v "nuscenes-path:/data" \
   image-name \
   /bin/bash
@@ -10,6 +10,6 @@ docker run \
 ### -it (run iteratively)
 ### --gpus all (use all GPUS. You can also select one)
 ### -v (mounted folders. The first is the code folder, 
-###     and the second the data. You should change nuscenes-path
+###     and the second is the data. You should change nuscenes-path
 ###     variable using your path)
 ### image-name (it is the name you gave when you built the image)
