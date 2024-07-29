6f9589e78b60b2b4eeb722537424c0be5afa5c43
RuanBispo <61762635+RuanBispo@users.noreply.github.com>
2024-07-29 07:30:12 -0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="radarnet-smalldet3d",
+    version="0.9.9",
+    description="Official codebase",
+    author="Ruan Bispo, Bruno Borges, Valdir Grassi Jr.",
+    author_email="ruanrobert00@usp.br, brunoborges@usp.br, vgrassi@usp.br",
+    license="MIT License",
+    packages=find_packages(),
+)
