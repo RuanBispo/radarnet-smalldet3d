4aa7cdac00086bb77b8787f0ec397ec54276172b
RuanBispo <61762635+RuanBispo@users.noreply.github.com>
2024-08-05 15:39:23 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.9.9",
     description="Official codebase",
     author="Ruan Bispo, Bruno Borges, Valdir Grassi Jr.",
-    author_email="ruanrobert00@usp.br, brunoborges@usp.br, vgrassi@usp.br",
+    author_email="ruanrobert00@usp.br, bborges.oliveira@usp.br, vgrassi@usp.br",
     license="MIT License",
     packages=find_packages(),
 )
